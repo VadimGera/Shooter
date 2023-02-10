e0b0aefe3fa9c3389c96750c440dd537e81ffb85
VadimGera <vadimnvidia@gmail.com>
2023-02-10 17:46:18 +0300

    Add Grenade And Script Grenade

---
+++ Assets/Scripts/Grenade.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using DefaultNamespace;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+
+public class Grenade : MonoBehaviour
+{
+    [SerializeField]public float delay = 3f;
+    [SerializeField]public float radius = 5f;
+    [SerializeField]public float force = 700f;
+    [SerializeField]public float damage = 80f;
+    
+    
+    public GameObject explosionEffect;
+    
+    private float countdown;
+    private bool hasExploded = false;
+    
+    void Start()
+    {
+        countdown = delay;
+    }
+
+   
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        if (countdown <= 0f && !hasExploded)
+        {
+            Explode();
+            hasExploded = true;
+        }
+    }
+
+    void Explode()
+    {    
+        //Спавн эффекта взрыва
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+        //Получение ближайших объектов
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+        foreach (Collider nearbyObject in colliders)
+        {
+            //Находим на объектах Rigidbody
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                //Если находим, добавляем силу направленную от гранаты
+                rb.AddExplosionForce(force, transform.position, radius);
+                
+            }
+
+             var destructible = nearbyObject.GetComponent<DestructibleObject>();
+             if (destructible != null)
+             {
+                 destructible.ReceiveDamage(damage);
+             }
+           
+            
+        }
+        
+        
+        Destroy(gameObject);
+    }
+}   
