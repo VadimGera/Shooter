dc2258d89758e404a413f4fd542b024f73536e55
VadimGera <vadimnvidia@gmail.com>
2023-02-10 14:55:54 +0300

    Add Weapon, Crosshair and Scripts

---
+++ Assets/Scripts/DestructibleObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class DestructibleObject : MonoBehaviour
+    {
+        [SerializeField] private float hpCurrent = 100;
+
+
+
+
+        public void ReceiveDamage(float damage)
+        {
+            hpCurrent -= damage;
+
+
+            if (hpCurrent < 0f)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Weapon : MonoBehaviour
+    {
+        [SerializeField] private float force = 4;
+        [SerializeField] private float damage = 4;
+        [SerializeField] private GameObject impactPrefab;
+        [SerializeField] private Transform shootPoint;
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (Physics.Raycast(shootPoint.position, shootPoint.forward, out var hit))
+                {
+                    print(hit.transform.gameObject.name);
+
+                    Instantiate(impactPrefab, hit.point, Quaternion.LookRotation(hit.normal, Vector3.up));
+
+                    var destructible = hit.transform.GetComponent<DestructibleObject>();
+
+                    if (destructible != null)
+                    {
+
+                        destructible.ReceiveDamage(damage);
+                    }
+
+                    var rigidbody = hit.transform.GetComponent<Rigidbody>();
+
+                    if (rigidbody != null)
+                    {
+                        rigidbody.AddForce(shootPoint.forward * force, ForceMode.Impulse);
+                    }
+                }
+            }
+        }
+    }   
+}
